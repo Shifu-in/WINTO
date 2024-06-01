0dccbd9733f92a93bae68cd979105eb077a763e5
KKTTZZ <etaskinova@yandex.ru>
2024-06-01 23:59:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,38 +29,6 @@ async function saveAndGoBack() {
     }
 }
 
-async function loadUserData() {
-    try {
-        console.log("Loading data for user_id:", userId);
-        const response = await fetch(`/get_user_data?user_id=${userId}`);
-        const data = await response.json();
-        balance = data.balance || 0;
-        clicks = data.clicks || 0;
-        document.getElementById('balance').innerText = balance;
-        document.getElementById('clicks').innerText = clicks;
-        console.log("Data loaded:", data);
-    } catch (error) {
-        console.error('Error loading data:', error);
-    }
-}
-
-async function saveUserData() {
-    try {
-        console.log("Saving data for user_id:", userId, "Balance:", balance, "Clicks:", clicks);
-        const response = await fetch('/update_user_data', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ user_id: userId, balance: balance, clicks: clicks })
-        });
-        const data = await response.json();
-        console.log('Data saved:', data);
-    } catch (error) {
-        console.error('Error saving data:', error);
-    }
-}
-
 document.addEventListener('DOMContentLoaded', (event) => {
     if (userId) {
         startApp();
