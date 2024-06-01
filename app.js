32ffc3866b2cd15ef136115c972eda1add9d39ff
KKTTZZ <etaskinova@yandex.ru>
2024-06-01 19:24:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-let balance = 0;const urlParams = new URLSearchParams(window.location.search);
+let balance = 0;
+const urlParams = new URLSearchParams(window.location.search);
 const userId = urlParams.get('user_id');
-function startApp() {    document.getElementById('start-app').classList.add('hidden');
-    document.getElementById('app').classList.remove('hidden');}
+
+function startApp() {
+    document.getElementById('start-app').classList.add('hidden');
+    document.getElementById('app').classList.remove('hidden');
+}
+
 function clickHandler() {
-    balance += 1;    document.getElementById('balance').innerText = balance;
-    saveBalanceToServer(balance);}
+    balance += 1;
+    document.getElementById('balance').innerText = balance;
+    saveBalanceToServer(balance);
+}
+
 async function saveBalanceToServer(newBalance) {
-    try {        const response = await fetch(`/update_balance?user_id=${userId}`, {
-            method: 'POST',            headers: {
-                'Content-Type': 'application/json'            },
-            body: JSON.stringify({ balance: newBalance })        });
-        const data = await response.json();        console.log('Balance saved:', data);
-    } catch (error) {        console.error('Error saving balance:', error);
-    }}
+    try {
+        const response = await fetch(`/update_balance?user_id=${userId}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ balance: newBalance })
+        });
+        const data = await response.json();
+        console.log('Balance saved:', data);
+    } catch (error) {
+        console.error('Error saving balance:', error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
-    if (userId) {        startApp();
-    }});
+    if (userId) {
+        startApp();
+    }
+});
