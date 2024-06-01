16eddcc0a37d060fc30f8bdc77e4fa74a05128f2
KKTTZZ <etaskinova@yandex.ru>
2024-06-02 00:48:43 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,40 @@
-const { json, send } = require('micro');const { router, get, post } = require('microrouter');
-const fs = require('fs');const path = require('path');
+const { json, send } = require('micro');
+const { router, get, post } = require('microrouter');
+const fs = require('fs');
+const path = require('path');
+
 const DATA_FILE = path.resolve(__dirname, 'user_data.json');
+
 function loadData() {
-    if (fs.existsSync(DATA_FILE)) {        const data = fs.readFileSync(DATA_FILE, 'utf8');
-        return JSON.parse(data);    }
-    return {};}
-function saveData(data) {
-    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');}
-async function getUserData(req, res) {
-    const { user_id } = req.query;    const data = loadData();
-    const user = data[user_id] || { balance: 0, clicks: 0 };    res.setHeader('Content-Type', 'application/json');
-    send(res, 200, user);}
-async function updateUserData(req, res) {
-    const newUser = await json(req);    const data = loadData();
-    data[newUser.user_id] = { balance: newUser.balance, clicks: newUser.clicks };    saveData(data);
-    res.setHeader('Content-Type', 'application/json');    send(res, 200, { status: 'success' });
+    if (fs.existsSync(DATA_FILE)) {
+        const data = fs.readFileSync(DATA_FILE, 'utf8');
+        return JSON.parse(data);
+    }
+    return {};
 }
-module.exports = router(    get('/get_user_data', getUserData),
-    post('/update_user_data', updateUserData));
+
+function saveData(data) {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
+}
+
+async function getUserData(req, res) {
+    const { user_id } = req.query;
+    const data = loadData();
+    const user = data[user_id] || { balance: 0, clicks: 0 };
+    res.setHeader('Content-Type', 'application/json');
+    send(res, 200, user);
+}
+
+async function updateUserData(req, res) {
+    const newUser = await json(req);
+    const data = loadData();
+    data[newUser.user_id] = { balance: newUser.balance, clicks: newUser.clicks };
+    saveData(data);
+    res.setHeader('Content-Type', 'application/json');
+    send(res, 200, { status: 'success' });
+}
+
+module.exports = router(
+    get('/get_user_data', getUserData),
+    post('/update_user_data', updateUserData)
+);
