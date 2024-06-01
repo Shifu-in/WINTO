6cf92efc51fc78271564b1b27a7737abc3778052
KKTTZZ <etaskinova@yandex.ru>
2024-06-02 00:08:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 let balance = 0;
 let clicks = 0;
 const urlParams = new URLSearchParams(window.location.search);
@@ -13,9 +12,7 @@ function startApp() {
 
 function clickHandler() {
     clicks += 1;
-    balance += 1;  // Увеличиваем баланс на 1 за каждый клик
     document.getElementById('clicks').innerText = clicks;
-    document.getElementById('balance').innerText = balance;
     saveUserData();
 }
 
@@ -29,13 +26,45 @@ async function saveAndGoBack() {
     }
 }
 
+async function loadUserData() {
+    try {
+        console.log("Loading data for user_id:", userId);
+        const response = await fetch(`/get_user_data?user_id=${userId}`);
+        const data = await response.json();
+        balance = data.balance || 0;
+        clicks = data.clicks || 0;
+        document.getElementById('balance').innerText = balance;
+        document.getElementById('clicks').innerText = clicks;
+        console.log("Data loaded:", data);
+    } catch (error) {
+        console.error('Error loading data:', error);
+    }
+}
+
+async function saveUserData() {
+    try {
+        console.log("Saving data for user_id:", userId, "Balance:", balance, "Clicks:", clicks);
+        const response = await fetch('/update_user_data', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ user_id: userId, balance: balance, clicks: clicks })
+        });
+        const data = await response.json();
+        console.log('Data saved:', data);
+    } catch (error) {
+        console.error('Error saving data:', error);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', (event) => {
     if (userId) {
         startApp();
     }
 });
 
-// Запрещаем двойной тап и масштабирование
+// Запрет двойного тапа и масштабирования
 document.addEventListener('gesturestart', function (e) {
     e.preventDefault();
 });
