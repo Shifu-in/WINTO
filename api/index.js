62050579f108d3b6b59712480b4038dfafec46fe
KKTTZZ <etaskinova@yandex.ru>
2024-06-02 00:48:04 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,38 +1,20 @@
-const { json, send } = require('micro');
-const { router, get, post } = require('microrouter');
-const fs = require('fs');
-const path = require('path');
-
+const { json, send } = require('micro');const { router, get, post } = require('microrouter');
+const fs = require('fs');const path = require('path');
 const DATA_FILE = path.resolve(__dirname, 'user_data.json');
-
 function loadData() {
-    if (fs.existsSync(DATA_FILE)) {
-        const data = fs.readFileSync(DATA_FILE, 'utf8');
-        return JSON.parse(data);
-    }
-    return {};
-}
-
+    if (fs.existsSync(DATA_FILE)) {        const data = fs.readFileSync(DATA_FILE, 'utf8');
+        return JSON.parse(data);    }
+    return {};}
 function saveData(data) {
-    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
-}
-
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');}
 async function getUserData(req, res) {
-    const { user_id } = req.query;
-    const data = loadData();
-    const user = data[user_id] || { balance: 0, clicks: 0 };
-    send(res, 200, user);
-}
-
+    const { user_id } = req.query;    const data = loadData();
+    const user = data[user_id] || { balance: 0, clicks: 0 };    res.setHeader('Content-Type', 'application/json');
+    send(res, 200, user);}
 async function updateUserData(req, res) {
-    const newUser = await json(req);
-    const data = loadData();
-    data[newUser.user_id] = { balance: newUser.balance, clicks: newUser.clicks };
-    saveData(data);
-    send(res, 200, { status: 'success' });
+    const newUser = await json(req);    const data = loadData();
+    data[newUser.user_id] = { balance: newUser.balance, clicks: newUser.clicks };    saveData(data);
+    res.setHeader('Content-Type', 'application/json');    send(res, 200, { status: 'success' });
 }
-
-module.exports = router(
-    get('/get_user_data', getUserData),
-    post('/update_user_data', updateUserData)
-);
+module.exports = router(    get('/get_user_data', getUserData),
+    post('/update_user_data', updateUserData));
